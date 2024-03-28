7d50007e21a143e90eab06ad9c304e2d1bb647b7
Joe Miehl <joe.miehl@gmail.com>
2024-03-28 07:58:29 -0400

    Initial Commit

---
+++ Goalie Statistics/Goalie_StatisticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Goalie_StatisticsApp.swift
+//  Goalie Statistics
+//
+//  Created by Joe Miehl on 3/28/24.
+//
+
+import SwiftUI
+
+@main
+struct Goalie_StatisticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
